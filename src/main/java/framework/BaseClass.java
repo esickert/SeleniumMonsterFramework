ac424c6f06f1f51264fe40ab5e9aaf9360ca616d
Erich Sickert <esickert@gmail.com>
2018-08-08 12:51:24 -0700

    hinzufügen

--- src/main/java/framework/BaseClass.java
+++ src/main/java/framework/BaseClass.java
@@ -12,16 +12,12 @@ public class BaseClass {
     @BeforeClass
     public void setupApplication()
     {
-
         Reporter.log("=====Browser Session Started=====", true);
 
         System.setProperty("webdriver.chrome.driver", "c://SeleniumDrivers//chromedriver.exe");
         //       WebDriver driver = new ChromeDriver();
-
-        driver=new ChromeDriver();
-
+        driver=new ChromeDriver();////////////////////
         driver.manage().window().maximize();
-
         driver.get("https://login20.monster.com/Login/SignIn?re=swoop&ch=MONS&intcid=skr_swoop_h1&r=http%3A%2F%2Fhome.monster.com%2F");
 
         Reporter.log("=====Application Started=====", true);
